38abc223db1f3a507e897154e93d47b8e73688dc
aryankitj <149313704+aryankitj@users.noreply.github.com>
2024-09-19 01:15:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+// let person = prompt("Please enter your name", "Harry Potter");
+// let text;
+// if (person == null || person == "") {
+//     text = "User cancelled the prompt.";
+// } else {
+//     text = "Hello " + person + "! How are you today?";
+// }
+
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
